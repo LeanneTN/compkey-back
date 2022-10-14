731dbc8c80a447be04408b0f5eb9635ea1d395e4
LeanneTN <2321604632@qq.com>
2022-10-14 17:52:37 +0800

    init repo

---
+++ src/main/java/com/example/compkeyback/CompkeyBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.compkeyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompkeyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompkeyBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/compkeyback/CompkeyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.compkeyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompkeyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
