584f0c0a64158c96705609b0d552b37640a8c7b7
yi_jin <1837935416@qq.com>
2022-11-15 00:57:05 +0800

    动态更新&&mylist

---
+++ src/main/java/com/example/compkeyback/dto/CompList.java
@@ -0,0 +1,10 @@
+package com.example.compkeyback.dto;
+
+import lombok.Data;
+
+@Data
+public class CompList {
+    private String seedWord;
+    private String compWord;
+    private double comp;
+}
